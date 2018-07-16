7db81ef785971e2d7af13b28949063d594153d6e
Luke Brady <lbrad23105@gmail.com>
2018-07-16 14:25:10 -0400

    Working on adding choosing your school and then loggin into it.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- udeploy.py
+++ udeploy.py
@@ -4,18 +4,20 @@ from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-web_object = {'authenticated' : False}
+web_object = {'authenticated' : False, 'school': None}
 
 @app.route('/')
 def index():
-    if web_object['authenticated'] is not True:
+    if web_object['school'] is None:
+        return redirect('/school')
+    elif web_object['authenticated'] is not True:
         return redirect('/login')
     else:
-        return render_template('index.html', university="UNG")
+        return render_template('index.html', school = web_object['school'])
 
 @app.route('/login')
 def login():
-    return render_template('login.html')   
+    return render_template('login.html', school = web_object['school'])   
 
 @app.route('/login', methods = ['POST'])
 def verify():
@@ -29,7 +31,18 @@ def verify():
         return redirect('/')
     else:
         return redirect('/login')
-    
+
+@app.route('/school')
+def school():
+    return render_template('school.html')
+
+@app.route('/school', methods =['POST'])
+def choose():
+    school = request.form['school']
+    # Add the school to the web object.
+    web_object['school'] = school
+    # Now return the login page for that University.
+    return redirect('/login')    
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
