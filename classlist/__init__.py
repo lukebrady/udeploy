05e318a74723653e78db67d0be78921a8ce62144
Luke Brady <lbrad23105@gmail.com>
2018-07-16 16:15:55 -0400

    Working on listing out class members for verification.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- classlist/__init__.py
+++ classlist/__init__.py
@@ -1,12 +1,13 @@
 import csv
 
 def parse_classlist(csv_list):
+    class_arr = []
     # Read the CSV from disk after being uploaded.
     with open(csv_list, newline = '') as classlist:
         classl = csv.reader(classlist, delimiter = ',')
         for c in classl:
-            print(c)
-    return classl
+            class_arr = c
+    return class_arr
 
 
 if __name__ == '__main__':
